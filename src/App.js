6843a51f9fb63ff37937b99a0c5fd99330c1cdae
SAIEF BRAHIM <100806401+SAIEFIBRAHIM@users.noreply.github.com>
2022-06-12 15:29:39 +0100

    Finishing JSX Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import imageInSrc from './imageInSrc.png'
+import './style.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div style={{border:'solid 1px black',maxWidth:'100vw'}}>
+        <h1 className="title red">Saief Brahim</h1>
+        <br />
+        <img style={{width:150}} src={imageInSrc} alt='img1' />
+        <br />
+        <img style={{width:150}} src="/imageInPublic.png" alt='img2' />
+        </div>
+        
+        <video style={{width:320 , height:240}}  controls autoPlay loop muted>
+        <source src="/gmcVideo.mp4" type="video/mp4" />
+        </video>
     </div>
   );
 }
